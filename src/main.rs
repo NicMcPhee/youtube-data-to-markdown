844e684160493a6cc7764cb6328101790445850c
Nic McPhee <mcphee@morris.umn.edu>
2023-01-06 22:11:00 -0600

    Set it to process all the video files every time we run it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use youtube_data::{playlist::Video, parsing::get_videos};
 
 fn main() {
+    let videos: Vec<Video> = get_videos("data/eps_page_1.json").unwrap();
+    for video in videos {
+        println!("{}", Video::filename(&video.title()));
+        video.write_markdown_file();
+    }
+
     let videos: Vec<Video> = get_videos("data/eps_page_2.json").unwrap();
     for video in videos {
         println!("{}", Video::filename(&video.title()));
